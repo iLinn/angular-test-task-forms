61770bebb5dbb6f90064257f3e6765b4279f6718
iLinn <11552398+iLinn@users.noreply.github.com>
2025-03-13 16:53:05 +0200

    Shared data added

---
+++ src/app/shared/enum/country.ts
@@ -0,0 +1,11 @@
+export enum Country {
+  Ukraine = 'Ukraine',
+  Poland = 'Poland',
+  Australia = 'Australia',
+  Austria = 'Austria',
+  Nepal = 'Nepal',
+  USA = 'USA',
+  Mexico = 'Mexico',
+}
+
+export const COUNTRIES = Object.values(Country);
